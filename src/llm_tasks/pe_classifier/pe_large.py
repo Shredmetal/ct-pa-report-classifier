a8655bc7d87b2e53fc4cb0372a5b1ac90a9b27ba
Morgan <morganj.lee01@gmail.com>
2025-06-27 19:11:16 +0800

    Refactored prompts since Gemma3 does not support system prompts. Used markdown fromatting and wrote the pe_large prompt

---
+++ src/llm_tasks/pe_classifier/pe_large.py
@@ -0,0 +1,36 @@
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+
+
+class PulmonaryEmbolusLarge:
+
+    @staticmethod
+    def get_pulmonary_embolus_large_prompts() -> ChatPromptTemplate:
+
+        human_message = HumanMessagePromptTemplate.from_template(
+            """
+            # Your Role
+            You are an expert medical assistant. Your job is to determine if the radiologist has reported whether the 
+            pulmonary embolus or pulmonary embolism is large or not, in the patient's radiology report which 
+            you will be provided with. Another system has already determined that the radiology report discloses the 
+            existence of the pulmonary embolus or pulmonary embolism.
+
+            # Critical Answering Instructions
+            **Important**: You can only respond with EXACTLY:
+            1. "true" if the radiologist has reported that pulmonary embolus or pulmonary embolism is large,
+            2. "false" if the radiologist has reported that pulmonary embolus or pulmonary embolism is **not** large,
+            3. "unknown" if there is insufficient information for you to make a determination.
+
+            # Risk Warning
+            Remember to answer only with 'true', 'false', or  'unknown' as failing to do so can cause serious and 
+            irreparable harm to the patient. **DO NOT DEVIATE FROM  THIS FORMAT - IT IS A LIFE AND DEATH SITUATION.**
+            Any other type of response is a critical failure of your function of ensuring that humans do not come to
+            harm.
+            
+            # Report For Your Analysis:
+            {report}
+            """
+        )
+
+        chat_prompt = ChatPromptTemplate.from_messages([human_message])
+
+        return chat_prompt
--- src/llm_tasks/pe_classifier/pe_presence.py
+++ src/llm_tasks/pe_classifier/pe_presence.py
@@ -5,7 +5,8 @@ class PulmonaryEmbolusPresence:
 
     @staticmethod
     def get_pulmonary_embolus_presence_prompts() -> ChatPromptTemplate:
-        system_message = SystemMessagePromptTemplate.from_template(
+
+        human_message = HumanMessagePromptTemplate.from_template(
             """
             # Your Role
             You are an expert medical assistant. Your job is to determine if the radiologist has reported 
@@ -19,20 +20,16 @@ class PulmonaryEmbolusPresence:
             3. "possible" if the radiologist indicates a probability but not a certainty of a a pulmonary embolus or pulmonary embolism.
 
             # Risk Warning
-            Any other type of response can cause serious and irreparable harm to the patient, which as an expert 
-            medical assistant, you must prevent."""
-        )
-
-        human_message = HumanMessagePromptTemplate.from_template(
-            """
-            Here is the radiology report: {report}
-
-            Based on this report, is there evidence of a pulmonary embolus or pulmonary embolism? Remember to answer 
-            only with 'true', 'false', or  'possible' as failing to do so can cause serious and irreparable harm to 
-            the patient. DO NOT DEVIATE FROM  THIS FORMAT - IT IS A LIFE AND DEATH SITUATION.
+            Remember to answer only with 'true', 'false', or  'possible' as failing to do so can cause serious and 
+            irreparable harm to the patient. **DO NOT DEVIATE FROM  THIS FORMAT - IT IS A LIFE AND DEATH SITUATION.**
+            Any other type of response is a critical failure of your function of ensuring that humans do not come to
+            harm.
+            
+            # Report For Your Analysis:
+            {report}
             """
         )
 
-        chat_prompt = ChatPromptTemplate.from_messages([system_message, human_message])
+        chat_prompt = ChatPromptTemplate.from_messages([human_message])
 
         return chat_prompt

a35ddcac7573fc55000334390605632dcf9b7f7d
Morgan <morganj.lee01@gmail.com>
2025-06-27 19:13:44 +0800

    Removed no-longer in use imports from prompt files

--- src/llm_tasks/pe_classifier/pe_presence.py
+++ src/llm_tasks/pe_classifier/pe_presence.py
@@ -1,4 +1,4 @@
-from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 
 
 class PulmonaryEmbolusPresence:

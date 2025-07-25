b13046f69d1032c4bfdd7fed24d73d07cff0912e
Shredmetal <morganj.lee01@gmail.com>
2025-07-25 16:36:39 +0100

    fixed data storage location (out of src), added /data to .gitignore

--- src/api/routers/processor_router_config.py
+++ src/api/routers/processor_router_config.py
@@ -6,7 +6,7 @@ from src.api.helpers.helpers_config import HelpersConfig
 
 @dataclass
 class ProcessorRouterConfig:
-    PROJECT_ROOT = Path(__file__).parent.parent.parent
+    PROJECT_ROOT = Path(__file__).parent.parent.parent.parent
     SOURCE_DIR = PROJECT_ROOT / "data" / "target"
     OUTPUT_DIR = PROJECT_ROOT / "data" / "output"
     ALLOWED_EXTENSIONS = HelpersConfig.ALLOWED_EXTENSIONS
